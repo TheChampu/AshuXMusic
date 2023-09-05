0d887b7e5c58b289742bc1f949feab3167a6725e
Venomxboss <142314403+Venomxboss@users.noreply.github.com>
2023-09-05 21:19:52 +0530

    Update thumbnails.py

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -48,32 +48,32 @@ async def get_thumb(videoid):
     url = f"https://www.youtube.com/watch?v={videoid}"
     results = VideosSearch(url, limit=1)
     for result in (await results.next())["result"]:
-    try:
-    title = result["title"]
-    title = re.sub("\W+", " ", title)
-    title = title.title()
-    except:
-    title = "Unsupported Title"
-    try:
-    duration = result["duration"]
-    except:
-    duration = "Unknown Mins"
-    thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-    try:
-    views = result["viewCount"]["short"]
-    except:
-    views = "Unknown Views"
-    try:
-    channel = result["channel"]["name"]
-    except:
-    channel = "Unknown Channel"
+        try:
+            title = result["title"]
+            title = re.sub("\W+", " ", title)
+            title = title.title()
+        except:
+            title = "Unsupported Title"
+        try:
+            duration = result["duration"]
+        except:
+            duration = "Unknown Mins"
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+        try:
+            views = result["viewCount"]["short"]
+        except:
+            views = "Unknown Views"
+        try:
+            channel = result["channel"]["name"]
+        except:
+            channel = "Unknown Channel"
 
     async with aiohttp.ClientSession() as session:
-    async with session.get(thumbnail) as resp:
-    if resp.status == 200:
-    f = await aiofiles.open(f"cache/thumb{videoid}.png", mode="wb")
-    await f.write(await resp.read())
-    await f.close()
+        async with session.get(thumbnail) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(f"cache/thumb{videoid}.png", mode="wb")
+                await f.write(await resp.read())
+                await f.close()
                 
     aaa= await app.get_profile_photos(app.id) 
     sp=await app.download_media(aaa[0]['file_id'],file_name=f'{app.id}.jpg')
@@ -130,8 +130,8 @@ async def get_thumb(videoid):
             font=arial,
         )
     try:
-    os.remove(f"cache/thumb{videoid}.png")
+        os.remove(f"cache/thumb{videoid}.png")
     except:
-    pass
+        pass
     background.save(f"cache/{videoid}.png")
     return f"cache/{videoid}.png"
